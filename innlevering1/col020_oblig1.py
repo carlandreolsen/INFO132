33e28ef642b52366dbabaa626ac9b759a41f6df6
Carl Andre <carl.andre.olsen@gmail.com>
2018-08-26 18:44:04 +0200

    innlevering 1 ferdig

--- innlevering1/col020_oblig1.py
+++ innlevering1/col020_oblig1.py
@@ -1,12 +1,18 @@
 #variabelen n er lagret i primærlageret. 
 #col020_oblig1.py er lagret i sekundærlageret.
 #python er en interpreter.
-#Erstatt verdi bundet til n for å skrive ut summen av de n første Fibonacci tallene
-n = 1234
+#Erstatt verdi bundet til n for å skrive ut summen av de n første Fibonacci tallene.
+#Antall elemtener du klarer å legge sammen med denne algoritmen er avhengig av hvor mye primærminne du har tilgjengelig.
+n = 12345
+
 #Legger til et ekstra listelement, 0, på starten for å slippe å gjøre unntak for første iterasjon.
 fibonacciListOfNumbers = [0,1]
-for i in range(0,n-1):
-    fibonacciListOfNumbers.append(fibonacciListOfNumbers[i]+fibonacciListOfNumbers[i+1])
+try:
+    for i in range(0,n-1):
+        fibonacciListOfNumbers.append(fibonacciListOfNumbers[i]+fibonacciListOfNumbers[i+1])
+except:
+    print(i)
 #Fibonacci-rekken 1,1,2,3,5,8,13,21,34,...  
-#print(x)
+#print(fibonacciListOfNumbers)    
 print(sum(fibonacciListOfNumbers[0:n+1]))
+
