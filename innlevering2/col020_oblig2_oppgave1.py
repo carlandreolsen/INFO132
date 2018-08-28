01979711ed93e67758780b7a9477636ef4154f76
Carl Andre <carl.andre.olsen@gmail.com>
2018-08-28 21:07:35 +0200

    Oppgave 2

---
+++ innlevering2/col020_oblig2_oppgave1.py
@@ -0,0 +1,13 @@
+#Operatoren + mellom to tekststrenger legger de sammen til en streng.
+#Eksempel: "str"+"ing" = "string"
+#
+#Operatoren * mellom en tekststreng og et heltall n legger strengen sammen med seg selv n ganger.
+#Eksempel: "str,"*4 = "str,str,str,str,"
+mittTall = 123
+minTekst = str(mittTall)
+mittTall = mittTall*10+1
+minTekst = str(mittTall)
+print(mittTall)
+print(minTekst)
+print(mittTall*2)
+print(minTekst*2)
